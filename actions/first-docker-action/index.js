b7f895aa75026aae2ada12aa935b9c5e41730f96
d-pro <dmitry.f.prohorov@gmail.com>
2019-11-24 22:50:17 +0300

    update

--- actions/first-docker-action/index.js
+++ actions/first-docker-action/index.js
@@ -5,15 +5,15 @@ const fs = require("fs");
 
 async function run() {
   try {
-    const firstarg = core.getInput("first-arg");
-    console.log(`Hello ${firstarg}!`);
-    const secondarg = core.getInput("second-arg");
-    console.log(`Hello ${secondarg}!`);
-    // Get the JSON webhook payload for the event that triggered the workflow
-    const payload = JSON.stringify(github.context.payload, undefined, 2);
-    console.log(`The event payload: ${payload}`);
+    // const firstarg = core.getInput("first-arg");
+    // console.log(`Hello ${firstarg}!`);
+    // const secondarg = core.getInput("second-arg");
+    // console.log(`Hello ${secondarg}!`);
+    // // Get the JSON webhook payload for the event that triggered the workflow
+    // const payload = JSON.stringify(github.context.payload, undefined, 2);
+    // console.log(`The event payload: ${payload}`);
     console.log(process.cwd());
-    let files = await fs.promises.readdir(procwss.cwd());
+    let files = await fs.promises.readdir(process.cwd());
     console.log("current dir", files);
     console.log(path.resolve(process.cwd(), ".."));
     files = await fs.promises.readdir(path.resolve(process.cwd(), ".."));
