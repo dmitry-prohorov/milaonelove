e24cbd4ce350cc4e7c0f9e83ba85de2f023c52ce
d-pro <dmitry.f.prohorov@gmail.com>
2019-11-24 22:57:40 +0300

    update

--- actions/first-docker-action/dist/index.js
+++ actions/first-docker-action/dist/index.js
@@ -528,15 +528,15 @@ const fs = __webpack_require__(747);
 
 async function run() {
   try {
-    const firstarg = core.getInput("first-arg");
-    console.log(`Hello ${firstarg}!`);
-    const secondarg = core.getInput("second-arg");
-    console.log(`Hello ${secondarg}!`);
-    // Get the JSON webhook payload for the event that triggered the workflow
-    const payload = JSON.stringify(github.context.payload, undefined, 2);
-    console.log(`The event payload: ${payload}`);
+    // const firstarg = core.getInput("first-arg");
+    // console.log(`Hello ${firstarg}!`);
+    // const secondarg = core.getInput("second-arg");
+    // console.log(`Hello ${secondarg}!`);
+    // // Get the JSON webhook payload for the event that triggered the workflow
+    // const payload = JSON.stringify(github.context.payload, undefined, 2);
+    // console.log(`The event payload: ${payload}`);
     console.log(process.cwd());
-    let files = await fs.promises.readdir(procwss.cwd());
+    let files = await fs.promises.readdir(process.cwd());
     console.log("current dir", files);
     console.log(path.resolve(process.cwd(), ".."));
     files = await fs.promises.readdir(path.resolve(process.cwd(), ".."));
@@ -544,6 +544,7 @@ async function run() {
     console.log(path.resolve(process.cwd(), "..", ".."));
     files = await fs.promises.readdir(path.resolve(process.cwd(), "..", ".."));
     console.log("grand parent dir", files);
+    core.setOutput("project-structure", files);
   } catch (error) {
     core.setFailed(error.message);
   }
