4c3e0b3cf94bdf3a3de3176baea9696a4f0c82a8
d-pro <dmitry.f.prohorov@gmail.com>
2019-11-25 21:23:37 +0300

    update

--- actions/setupsfdx/src/installer.ts
+++ actions/setupsfdx/src/installer.ts
@@ -1,12 +1,13 @@
 import * as os from "os";
 import * as path from "path";
-import * as fs from "fs";
 import * as semver from "semver";
 import { exec as childExec } from "child_process";
 import { promisify } from "util";
+
 let tempDirectory = process.env["RUNNER_TEMP"] || "";
 let osPlat: string = os.platform();
 // current latest sfdx cli version
+// TODO: find a way to get these version via http
 const DEFAULT_LATEST_VERSION = "7.33.2-045d48473e";
 const execa = promisify(childExec);
 
@@ -34,20 +35,14 @@ import * as tc from "@actions/tool-cache";
 import * as io from "@actions/io";
 
 export async function getSfdxCli() {
-  console.log(tc.findAllVersions("sfdx"));
-  console.log(
-    await fs.promises.readdir(
-      path.resolve(<string>process.env["RUNNER_TOOL_CACHE"])
-    )
-  );
-
+  // find latest stored version
   let latestVersion = tc.findAllVersions("sfdx").reduce((result, version) => {
     if (semver.lt(result, version)) {
       result = version;
     }
     return result;
   }, DEFAULT_LATEST_VERSION);
-  core.debug(`Latest version found is: ${latestVersion}`);
+  core.debug(`Latest stored version is: ${latestVersion}`);
 
   // always check latest version
   let toolPath = tc.find("sfdx", latestVersion);
@@ -63,7 +58,7 @@ export async function getSfdxCli() {
 
   //
   // a tool installer initimately knows details about the layout of that tool
-  // for example, node binary is in the bin folder after the extract on Mac/Linux.
+  // for example, sfdx binary is in the bin folder after the extract on Mac/Linux.
   // layouts could change by version, by platform etc... but that's the tool installers job
   //
   if (osPlat != "win32") {
@@ -110,7 +105,7 @@ async function acquireSfdxCli(versionSpec: string): Promise<string> {
   }
 
   //
-  // Install into the local tool cache - node extracts with a root folder that matches the fileName downloaded
+  // Install into the local tool cache - sfdx extracts with a root folder that matches the fileName downloaded
   //
   let toolRoot = path.join(extPath, fileName);
   return await tc.cacheDir(toolRoot, "sfdx", versionSpec);
@@ -131,13 +126,14 @@ async function updateCli(): Promise<void> {
 async function cleanVersions(latestVersion: string): Promise<void> {
   if (!latestVersion) return;
 
-  core.debug("Clean old versions");
+  core.debug("Find old versions");
   const versionsToDelete = tc
     .findAllVersions("sfdx")
     .filter(
       version => version !== latestVersion && version !== DEFAULT_LATEST_VERSION
     );
 
+  core.debug(`Clear old versions: ${versionsToDelete}`);
   for (let i = 0; i <= versionsToDelete.length; i++) {
     const versionPath = tc.find("sfdx", versionsToDelete[i]);
     if (versionPath) {
@@ -146,12 +142,12 @@ async function cleanVersions(latestVersion: string): Promise<void> {
   }
 }
 
-// map arch to download dist url format https://developer.salesforce.com/media/salesforce-cli
+// map arch to download dist url format
+// @see https://developer.salesforce.com/docs/atlas.en-us.sfdx_setup.meta/sfdx_setup/sfdx_setup_install_cli.htm
 function translateArchToDistUrl(arch: string): string {
   switch (arch) {
     case "arm64":
     case "x64":
-    case "ppc64":
       return "x64";
     case "arm":
       return "arm";
